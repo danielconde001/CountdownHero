4cd9427ec859f27fe38c97d6f3d16fa50c856ac0
idris331 <mohammed.idris5640@gmail.com>
2026-07-25 19:41:59 +0300

    Updated SceneLoadTrigger to use 2D version

--- Assets/SceneLoader/Scripts/SceneLoadTrigger.cs
+++ Assets/SceneLoader/Scripts/SceneLoadTrigger.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class SceneLoadTrigger : MonoBehaviour
 {
     [SerializeField] private SceneField _sceneToLoad;
 
     [HideInInspector][SerializeField] private Transform _transfrom;
-    [HideInInspector][SerializeField] private BoxCollider _boxCollider;
+    [HideInInspector][SerializeField] private BoxCollider2D _boxCollider;
 
 
     private const string _playerTag = "Player";
@@ -16,12 +16,12 @@ public class SceneLoadTrigger : MonoBehaviour
     {
         _transfrom = this.transform;
 
-        _boxCollider = this.GetComponent<BoxCollider>();
+        _boxCollider = this.GetComponent<BoxCollider2D>();
         _boxCollider.isTrigger = true;
     }
 
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag != _playerTag)
             return;
@@ -33,7 +33,7 @@ public class SceneLoadTrigger : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.black;
-        Gizmos.matrix = Matrix4x4.TRS(_transfrom.position, _transfrom.rotation, _transfrom.lossyScale);
-        Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+        Gizmos.matrix = Matrix4x4.TRS(_transfrom.position, _transfrom.rotation, Vector3.one);
+        Gizmos.DrawWireCube(Vector3.zero, new Vector3(_transfrom.lossyScale.x, _transfrom.lossyScale.y, 0f));
     }
 }
