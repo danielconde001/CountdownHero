54ea02550f64209546c409f8504894110f1a2dab
danielconde001 <danielconde001@yahoo.com>
2026-07-25 15:15:26 +0800

    death zone

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Respawns the player when they enter a hazard volume, with scene reload as a fallback.
+/// </summary>
+[RequireComponent(typeof(Collider2D))]
+public class DeathZone : MonoBehaviour
+{
+    [SerializeField] private bool reloadSceneIfNoRespawn = true;
+
+    private void Awake()
+    {
+        GetComponent<Collider2D>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerRespawn playerRespawn = other.GetComponentInParent<PlayerRespawn>();
+        if (playerRespawn != null)
+        {
+            playerRespawn.Respawn();
+            return;
+        }
+
+        if (reloadSceneIfNoRespawn)
+        {
+            Scene activeScene = SceneManager.GetActiveScene();
+            string sceneToReload = string.IsNullOrEmpty(activeScene.path)
+                ? activeScene.name
+                : activeScene.path;
+            SceneManager.LoadScene(sceneToReload);
+        }
+    }
+}
